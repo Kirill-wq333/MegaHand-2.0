54cd764883a390e5f8bac69ed1f3299c836c0a6e
KirillSharonov <154874212+Kirill-wq333@users.noreply.github.com>
2024-12-23 15:35:08 +0400

    Added Content

--- app/src/main/java/com/evothings/mhand/presentation/feature/navigation/bottomBar/ui/bottomsheet/OtherBottomSheet.kt
+++ app/src/main/java/com/evothings/mhand/presentation/feature/navigation/bottomBar/ui/bottomsheet/OtherBottomSheet.kt
@@ -10,17 +10,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.evothings.mhand.R
+import com.evothings.mhand.presentation.theme.MegahandTheme
 import com.evothings.mhand.presentation.theme.MegahandTypography
 import com.evothings.mhand.presentation.theme.paddings
 import com.evothings.mhand.presentation.theme.spacers
@@ -29,7 +33,9 @@ import com.evothings.mhand.presentation.theme.values.MegahandShapes
 @Preview
 @Composable
 fun OtherBottomSheetPreview(){
-    OtherBottomSheet()
+    MegahandTheme {
+        OtherBottomSheet()
+    }
 }
 
 
@@ -49,6 +55,13 @@ private fun Content(
     Box(
         modifier = Modifier
             .fillMaxWidth()
+            .background(
+                color = colorScheme.onSecondary,
+                shape = RoundedCornerShape(
+                    topStart = 12.dp,
+                    topEnd = 12.dp
+                )
+                )
     ){
         Column(
             modifier = Modifier
@@ -58,23 +71,42 @@ private fun Content(
                     start = MaterialTheme.paddings.extraGiant,
                     bottom = MaterialTheme.paddings.extraLarge,
                     end = MaterialTheme.paddings.extraGiant
-                ),
-
+                )
         ) {
+            Text(
+                text = stringResource(R.string.other),
+                textAlign = TextAlign.Start,
+                color = colorScheme.secondary,
+                style = MegahandTypography.headlineMedium,
+                modifier = Modifier
+            )
+            Spacer(modifier = Modifier.height(MaterialTheme.spacers.extraLarge))
             Column(
                 verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacers.small)
             ) {
                 RowBottomItems(
                     text = stringResource(R.string.favourites_screen_title),
                     text2 = stringResource(R.string.shops),
+                    modifier = Modifier
+                        .padding(horizontal = 41.5.dp),
+                    modifier2 = Modifier
+                        .padding(horizontal = 46.dp)
                 )
                 RowBottomItems(
                     text = stringResource(R.string.news_screen_title),
                     text2 = stringResource(R.string.about_service),
+                    modifier = Modifier
+                        .padding(horizontal = 51.5.dp),
+                    modifier2 = Modifier
+                        .padding(horizontal = 45.dp)
                 )
                 RowBottomItems(
                     text = stringResource(R.string.vacancies),
                     text2 = stringResource(R.string.help),
+                    modifier = Modifier
+                        .padding(horizontal = 47.dp),
+                    modifier2 = Modifier
+                        .padding(horizontal = 52.5.dp)
                 )
             }
             Spacer(modifier = Modifier.height(MaterialTheme.spacers.medium))
@@ -99,30 +131,24 @@ private fun Content(
 
 @Composable
 private fun RowBottomItems(
+    modifier: Modifier = Modifier,
+    modifier2: Modifier = Modifier,
     text: String,
     text2: String
 ) {
 
     Row(
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
         BottomItem(
             text = text,
-            modifier = Modifier
-                .padding(
-                    vertical = 42.dp,
-                    horizontal = MaterialTheme.paddings.extraLarge
-                )
+            modifier = modifier
         )
-        Spacer(modifier = Modifier.width(MaterialTheme.spacers.small))
         BottomItem(
             text = text2,
-            modifier = Modifier
-                .padding(
-                    vertical = 42.dp,
-                    horizontal = MaterialTheme.paddings.extraLarge
-                )
+            modifier = modifier2
         )
     }
 
@@ -147,6 +173,8 @@ private fun BottonFillMaxSizeItem(
             text = text,
             color = colorScheme.secondary,
             style = MegahandTypography.labelLarge,
+            modifier = Modifier
+                .padding(vertical = MaterialTheme.paddings.extraLarge)
         )
     }
 
@@ -171,6 +199,7 @@ private fun BottomItem(
             color = colorScheme.secondary,
             style = MegahandTypography.labelLarge,
             modifier = modifier
+                .padding(vertical = MaterialTheme.paddings.extraLarge)
         )
     }
 
