93c465604841fb3f9b48d972ace7a0e961e440da
KirillSharonov <154874212+Kirill-wq333@users.noreply.github.com>
2024-12-26 15:21:06 +0400

    Added content

--- app/src/main/java/com/evothings/mhand/presentation/feature/news/ui/ArticleScreen.kt
+++ app/src/main/java/com/evothings/mhand/presentation/feature/news/ui/ArticleScreen.kt
@@ -1,16 +1,31 @@
 package com.evothings.mhand.presentation.feature.news.ui
 
 import android.content.Intent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
 import com.evothings.domain.feature.news.model.NewsArticle
+import com.evothings.mhand.presentation.feature.news.ui.articleComponents.InformationArticle
 import com.evothings.mhand.presentation.feature.news.viewmodel.article.ArticleContract
 import com.evothings.mhand.presentation.feature.news.viewmodel.article.ArticleViewModel
+import com.evothings.mhand.presentation.theme.paddings
+import com.evothings.mhand.presentation.theme.spacers
+import com.evothings.mhand.presentation.theme.values.MegahandShapes
 
 data class ArticleUiState(
     val newsModel: NewsArticle?,
@@ -32,6 +47,7 @@ fun ArticleScreen(
     openAnotherArticle: (id: String) -> Unit,
     onBack: () -> Unit
 ) {
+
     val context = LocalContext.current
 
     val state by vm.state.collectAsStateWithLifecycle()
@@ -62,5 +78,62 @@ fun ArticleScreen(
 
         override fun openSimilarArticle(id: String) = openAnotherArticle(id)
 
+    }
+
+}
+
+@Composable
+private fun Content(
+    modifier: Modifier = Modifier,
+    mainImage: String,
+    title: String,
+    publicationDate: String,
+    informationNews: String,
+) {
+    LazyColumn {
+
+        item {
+            ImageNews(
+                mainImage = mainImage,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(214.dp)
+                    .padding(horizontal = MaterialTheme.paddings.extraLarge)
+            )
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(MaterialTheme.spacers.extraLarge))
+        }
+
+        item {
+            InformationArticle(
+                title = title,
+                publicationDate = publicationDate,
+                informationNews = informationNews,
+                onClick = { }
+            )
+        }
+
+    }
+}
+
+
+
+@Composable
+fun ImageNews(
+    modifier: Modifier = Modifier,
+    mainImage: String
+) {
+    Box(
+        modifier = modifier
+    ) {
+        AsyncImage(
+            model = mainImage,
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .clip(shape = MegahandShapes.extraLarge)
+        )
     }
 }
