079e8d543f9e0c8bf545343622c67983dae4c8d5
KirillSharonov <154874212+Kirill-wq333@users.noreply.github.com>
2025-01-14 15:56:40 +0400

    Added new OnboardingScreens

---
+++ app/src/main/java/com/evothings/mhand/presentation/feature/onboarding/ui/screen/CartOnboardingScreen.kt
@@ -0,0 +1,138 @@
+package com.evothings.mhand.presentation.feature.onboarding.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.evothings.domain.feature.product.model.Product
+import com.evothings.domain.util.Mock
+import com.evothings.mhand.presentation.feature.cart.ui.components.CartSelectionOptions
+import com.evothings.mhand.presentation.feature.cart.ui.components.productComponent.Cart
+import com.evothings.mhand.presentation.feature.onboarding.model.CardAlignment
+import com.evothings.mhand.presentation.feature.onboarding.model.Onboarding
+import com.evothings.mhand.presentation.feature.onboarding.ui.components.Onboarding
+import com.evothings.mhand.presentation.feature.onboarding.ui.components.OnboardingScrim
+import com.evothings.mhand.presentation.feature.onboarding.ui.components.onboardingItem
+import com.evothings.mhand.presentation.theme.MegahandTheme
+import com.evothings.mhand.presentation.theme.spacers
+
+@Composable
+fun CartOnboardingScreen(
+    modifier: Modifier = Modifier,
+    onFinish: () -> Unit
+) {
+    val itemPositionMap = remember {
+        mutableStateMapOf<Onboarding, Float>()
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Column {
+
+            CartSelectionOptions(
+                modifier = Modifier
+                    .padding(horizontal = MaterialTheme.spacers.medium),
+                selectAllChecked = false,
+                onCheckSelectAll = {},
+                onClear = {}
+            )
+            DemoProduct(
+                modifier = Modifier
+                    .padding(MaterialTheme.spacers.medium)
+                    .onboardingItem(
+                        itemsMap = itemPositionMap,
+                        key = Onboarding.Cart.CartItem,
+                        hide = true
+                    ),
+                product = Mock.demoProduct
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(MaterialTheme.spacers.small)
+            )
+            DemoProduct(
+                modifier = Modifier
+                    .padding(horizontal = MaterialTheme.spacers.medium),
+                product = Mock.demoProductsList[1]
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(MaterialTheme.spacers.large)
+            )
+            DemoProduct(
+                modifier = Modifier
+                    .padding(horizontal = MaterialTheme.spacers.medium),
+                product = Mock.demoProductsList[2]
+            )
+        }
+
+        OnboardingScrim(
+            onClick = onFinish
+        )
+
+        DemoProduct(
+            modifier = Modifier
+                .offset(
+                    y = (itemPositionMap[Onboarding.Cart.CartItem] ?: 0f).dp
+                )
+                .background(MaterialTheme.colorScheme.onSecondary)
+                .padding(MaterialTheme.spacers.medium),
+            product = Mock.demoProduct
+        )
+
+        with(Onboarding.Cart.CartItem) {
+            Onboarding(
+                icon = iconRes,
+                heading = title,
+                underHeading = description,
+                pageNumber = itemsCounter,
+                onFinish = true,
+                alignment = CardAlignment.BOTTOM
+            )
+        }
+
+    }
+}
+
+
+@Composable
+private fun DemoProduct(
+    modifier: Modifier,
+    product: Product
+) {
+    Cart(
+        modifier = modifier,
+        model = product,
+        isSelected = true,
+        onSelect = {},
+        onClick = {},
+        toggleFavourite = {},
+        removeFromCart = {}
+    )
+}
+
+@Preview
+@Composable
+private fun CartOnboardingPreview() {
+    MegahandTheme {
+        Surface {
+            CartOnboardingScreen(
+                onFinish = {}
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/evothings/mhand/presentation/feature/onboarding/ui/screen/OtherOnboardingScreen.kt
@@ -0,0 +1,100 @@
+package com.evothings.mhand.presentation.feature.onboarding.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.evothings.mhand.presentation.feature.navigation.bottomBar.ui.bottomsheet.Other
+import com.evothings.mhand.presentation.feature.navigation.bottomBar.ui.bottomsheet.OtherBottomSheet
+import com.evothings.mhand.presentation.feature.onboarding.model.CardAlignment
+import com.evothings.mhand.presentation.feature.onboarding.model.Onboarding
+import com.evothings.mhand.presentation.feature.onboarding.ui.components.BottomSheetLikeIndicator
+import com.evothings.mhand.presentation.feature.onboarding.ui.components.Onboarding
+import com.evothings.mhand.presentation.feature.onboarding.ui.components.OnboardingScrim
+import com.evothings.mhand.presentation.theme.MegahandTheme
+import com.evothings.mhand.presentation.theme.colorScheme.ColorTokens
+import com.evothings.mhand.presentation.theme.spacers
+
+@Composable
+fun OtherOnboardingScreen(onFinish: () -> Unit) {
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        OnboardingScrim(onClick = onFinish)
+
+        with(Onboarding.Other.BottomSheet) {
+            com.evothings.mhand.presentation.feature.onboarding.ui.components.Onboarding(
+                icon = iconRes,
+                heading = title,
+                underHeading = description,
+                onFinish = true,
+                alignment = CardAlignment.TOP,
+                visibleButtonTurnBack = false,
+                pageNumber = itemsCounter
+            )
+        }
+
+        DemoOtherBottomSheet(
+            modifier = Modifier.align(Alignment.BottomCenter)
+        )
+    }
+
+}
+
+@Composable
+private fun DemoOtherBottomSheet(modifier: Modifier) {
+
+    val isDarkMode = MaterialTheme.colorScheme.secondary != ColorTokens.Graphite
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colorScheme.onSecondary,
+                shape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp)
+            ),
+    ) {
+        BottomSheetLikeIndicator(
+            modifier = Modifier.align(Alignment.TopStart)
+        )
+        Other(
+            modifier = Modifier
+                .padding(
+                    vertical = 15.dp,
+                    horizontal = MaterialTheme.spacers.extraLarge
+                ),
+            couponAmount = 300,
+            darkMode = isDarkMode,
+            enableCouponButton = true,
+            openAppScreen = {},
+            openVacanciesPage = {},
+            openHelpPage = {},
+            onChangeTheme = {},
+            openCouponPhoneConfirmationScreen = {}
+        )
+    }
+
+}
+
+@Preview
+@Composable
+private fun OtherOnboardingPreview() {
+    MegahandTheme {
+        Surface {
+            OtherOnboardingScreen(
+                onFinish = {}
+            )
+        }
+    }
+}
