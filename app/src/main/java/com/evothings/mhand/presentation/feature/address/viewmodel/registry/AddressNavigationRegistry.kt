047869301f2e0cf6602808025f9391586ad78d14
KirillSharonov <154874212+Kirill-wq333@users.noreply.github.com>
2025-01-17 13:45:09 +0400

    Added new object in viewmodel

---
+++ app/src/main/java/com/evothings/mhand/presentation/feature/address/viewmodel/registry/AddressNavigationRegistry.kt
@@ -0,0 +1,19 @@
+package com.evothings.mhand.presentation.feature.address.viewmodel.registry
+
+import com.evothings.domain.feature.address.model.Address
+
+object AddressNavigationRegistry {
+
+    private var value: Address? = null
+
+    fun get(): Address? = value
+
+    fun set(details: Address) {
+        value = details
+    }
+
+    fun clear() {
+        value = null
+    }
+
+}
