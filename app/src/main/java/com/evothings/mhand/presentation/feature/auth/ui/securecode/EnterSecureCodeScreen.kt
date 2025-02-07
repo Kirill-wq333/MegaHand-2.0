82bd82d481427f0cbeebd101dfe4bea7b71ce506
KirillSharonov <154874212+Kirill-wq333@users.noreply.github.com>
2025-02-07 15:37:13 +0400

    A lot of Create and Fixed content

--- app/src/main/java/com/evothings/mhand/presentation/feature/auth/ui/securecode/EnterSecureCodeScreen.kt
+++ app/src/main/java/com/evothings/mhand/presentation/feature/auth/ui/securecode/EnterSecureCodeScreen.kt
@@ -1,7 +1,6 @@
 package com.evothings.mhand.presentation.feature.auth.ui.securecode
 
 import android.content.Context
-import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
